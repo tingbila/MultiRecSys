71d4a2ecee155e336d6134a999c7a276d3eaeaec
mingyang.zhang <mingyang.zhang@ushow.media>
2025-06-15 23:24:38 +0800

    ods_adtributor_add_metrics_df

--- project/指标波动归因分析/deepseek_promot.py
+++ project/指标波动归因分析/deepseek_promot.py
@@ -32,9 +32,9 @@ def prompt_con():
     今天整体指标增加了(降低了) X（用aft_sum-pre_sum），原因如下:
     1. 维度影响排序：A维度（surprise_sum = X）> B维度（surprise_sum = X）
     2. 维度内元素排序：
-       - 渠道维度：  a1（surprise 0.007697）> a2（surprise 0.001973）> a3（surprise 0.000725）
-       - 新老客维度：b1（surprise 0.000557）>  b2（surprise 0.000426）
-    注意：只输出结论，不要解释算法或过程。
+       - 渠道维度：  a1（surprise 0.007697）(before x -> after x) >  a2（surprise 0.001973）(before x -> after x)> a3（surprise 0.000725）(before x -> after x)
+       - 新老客维度：b1（surprise 0.000557）(before x -> after x) >  b2（surprise 0.000426）(before x -> after x)
+    注意：只输出结论，不要解释算法或过程，输出结果不要带有*这种特殊字符。
     """
 
     return [
